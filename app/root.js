4fce835d210025cc978f8a61c1cb67986d7940c5
DESKTOP-B9I8OOT\ROOKIOR <weiziyan@okwuyou.com>
2018-01-09 22:57:39 +0800

    音乐上一曲下一曲与剩余时间

--- app/root.js
+++ app/root.js
@@ -4,30 +4,75 @@ import Player from './page/player'
 import MusicList from './page/musiclist'
 import { MUSIC_LIST } from './config/musiclist'
 import { Router, IndexRoute, Link, Route, hashHistory } from 'react-router'
-
+import Pubsub from 'pubsub-js'
 
 let App = React.createClass({
 	getInitialState(){
 		return{
 			musicList : MUSIC_LIST,
-			currentMusicItem: MUSIC_LIST[1]
+			currentMusicItem: MUSIC_LIST[0]
 		}
+	},
+	playMusic(musicItem){
+		$('#player').jPlayer('setMedia', {
+			 mp3: musicItem.file
+		}).jPlayer('play');
+		
+		this.setState({
+			currentMusicItem:musicItem
+		});
+	},
+	playNext( type='next' ){
+		let index = this.findMusicIndex(this.state.currentMusicItem);
+		let newIndex = null;
+		let musicListLength = this.state.musicList.length
+		if(type === 'next'){
+			newIndex = (index + 1) % musicListLength;
+		}else{
+			newIndex = (index - 1 + musicListLength) % musicListLength;
+		}
+		
+		this.playMusic(this.state.musicList[newIndex]);
+	},
+	findMusicIndex(musicItem){
+		return this.state.musicList.indexOf(musicItem);
 	},
 	componentDidMount(){
 		$('#player').jPlayer({
-			ready: function(){
-				$('#player').jPlayer('setMedia',{
-					mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
-				}).jPlayer('play');
-			},
+			
 			supplied:'mp3',
 			wmode:'window'
 			
 		});
+		this.playMusic(this.state.currentMusicItem);
+		$('#player').bind($.jPlayer.event.ended, (e) => {
+			this.playNext();
+		});
+		Pubsub.subscribe('DELETE_MUSIC', (msg, musicItem) => {
+			this.setState({
+				musicList: this.state.musicList.filter(item => {
+					return item !== musicItem;
+				})
+			})
+		});
+		Pubsub.subscribe('PLAY_MUSIC', (msg, musicItem) => {
+			this.playMusic(musicItem);
+		});
+		Pubsub.subscribe('PLAY_PREV', (msg, musicItem) => {
+			this.playNext('prev');
+		});
+		Pubsub.subscribe('PLAY_NEXT', (msg, musicItem) => {
+			this.playNext();
+		});
+		
 		
 	},	
 	componentWillUnmount(){
-
+		Pubsub.unsubscribe('DELETE_MUSIC');
+		Pubsub.unsubscribe('PLAY_MUSIC');
+		Pubsub.unsubscribe('PLAY_PREV');
+		Pubsub.unsubscribe('PLAY_NEXT');
+		$('#player').unbind($.jPlayer.event.ended);
 	},
 	render(){
 		return(
